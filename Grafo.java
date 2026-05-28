e96de381253410db79727b7b46dcce74fcdaf6d4
Mateus <101527244+materoo@users.noreply.github.com>
2026-05-28 16:36:33 -0300

    Mudanças no Grafo, e GrafoListaAdjacencia completo

--- Grafo.java
+++ Grafo.java
@@ -1,50 +1,42 @@
+//Mateus Juares Felipe - 16891602
+//Jão Pedro Neves -
+
 abstract public class Grafo {
 
-    // Adiciona um novo vértice ao grafo, caso ele ainda não exista.
-    public void adicionarVertice(String vertice) {
+    protected int tamanho; // numero de arestas
+    protected int ordem; // numero de vertices
 
-    }
+    // Adiciona um novo vértice ao grafo, caso ele ainda não exista.
+    public abstract void adicionarVertice(String vertice);
 
     // Remove um vértice do grafo. Ao remover o vértice, todas as arestas incidentes
     // a ele também devem ser removidas.
-    public void removerVertice(String vertice) {
-
-    }
+    public abstract void removerVertice(String vertice);
 
     // Adiciona uma aresta entre dois vértices. Como o grafo é não direcionado, a
     // ligação deve ser registrada nos dois sentidos.
-    public void adicionarAresta(String origem, String destino) {
-
-    }
+    public abstract void adicionarAresta(String origem, String destino);
 
     // Remove a aresta entre dois vértices, caso ela exista.
-    public void removerAresta(String origem, String destino) {
-
-    }
+    public abstract void removerAresta(String origem, String destino);
 
     // Verifica se um determinado vértice pertence ao grafo.
-    public boolean existeVertice(String vertice) {
-
-    }
+    public abstract boolean existeVertice(String vertice);
 
     // Verifica se existe uma aresta entre dois vértices.
-    public boolean existeAresta(String origem, String destino) {
-
-    }
+    public abstract boolean existeAresta(String origem, String destino);
 
     // Retorna o grau de um vértice, isto é, o número de vértices adjacentes a ele.
-    public int grau(String vertice) {
-
-    }
+    public abstract int grau(String vertice);
 
     // Retorna a ordem do grafo, isto é, o número de vértices.
     public int ordem() {
-
+        return this.ordem;
     }
 
     // Retorna o tamanho do grafo, isto é, o número de arestas.
     public int tamanho() {
-
+        return this.tamanho;
     }
 
     // Retorna uma representação textual do grafo
@@ -52,8 +44,7 @@ abstract public class Grafo {
     // "A" -- "B"; -> sem peso
     // "A" -- "C" [label="3"]; -> com peso
     // }
-    public String toString() {
-
-    }
+    @Override
+    public abstract String toString();
 
 }
--- GrafoListaAdjacencia.java
+++ GrafoListaAdjacencia.java
@@ -1,8 +1,128 @@
+//Mateus Juares Felipe - 16891602
+//Jão Pedro Neves -
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class GrafoListaAdjacencia extends Grafo {
+
+    private Map<String, Set<String>> adjacencias;
+    // String = vértice / key;
+    // Set<String> = conjunto de vértices adjacentes a ele / value
+
+    public GrafoListaAdjacencia() {
+
+        adjacencias = new TreeMap<>();
+        tamanho = 0;
+        ordem = 0;
+    }
+
+    @Override
+    public void adicionarVertice(String vertice) {
+
+        if (!existeVertice(vertice)) {
+
+            adjacencias.put(vertice, new TreeSet<>()); // adiciona o vertice como key e um conjunto vazio como value
+            ordem++; // numero de vertices +1
+        }
+    }
+
+    @Override
+    public void removerVertice(String vertice) {
+
+        if (!existeVertice(vertice)) {
+            return;
+        }
+
+        for (String vizinho : adjacencias.get(vertice)) {
+            adjacencias.get(vizinho).remove(vertice);
+            tamanho--; // numero de arestas -1
+        }
+
+        adjacencias.remove(vertice);
+
+        ordem--; // numero de vertices -1
+    }
+
+    @Override
+    public void adicionarAresta(String origem, String destino) {
+
+        adicionarVertice(origem);
+        adicionarVertice(destino);
+
+        if (!existeAresta(origem, destino)) {
+
+            adjacencias.get(origem).add(destino);
+            adjacencias.get(destino).add(origem);
+
+            tamanho++; // numero de arestas +1
+        }
+    }
+
+    @Override
+    public void removerAresta(String origem, String destino) {
+
+        if (!existeAresta(origem, destino)) {
+            return;
+        }
+
+        adjacencias.get(origem).remove(destino);
+        adjacencias.get(destino).remove(origem);
+
+        tamanho--; // numero de arestas -1
+    }
+
+    @Override
+    public boolean existeVertice(String vertice) {
+
+        return adjacencias.containsKey(vertice); // verifica se a key "vertice" existe no Map adjacencias
+    }
+
+    @Override
+    public boolean existeAresta(String origem, String destino) {
+        if (!existeVertice(origem)) {
+            return false;
+        }
+
+        return adjacencias.get(origem).contains(destino); // verifica se o value da key "origem" contém o "destino"
+    }
+
+    @Override
+    public int grau(String vertice) {
+
+        if (!existeVertice(vertice)) {
+            return 0;
+        }
+
+        return adjacencias.get(vertice).size(); // retorna o numero de vertices adjacentes ao vertice
+    }
 
     @Override
     public String toString() {
 
-    }
+        StringBuilder sb = new StringBuilder(); // Função StringBuilder para construir a string de saída
 
+        sb.append("graph {\n");
+
+        for (String v1 : adjacencias.keySet()) {
+
+            for (String v2 : adjacencias.get(v1)) {
+
+                if (v1.compareTo(v2) < 0) {
+
+                    sb.append("\"")
+                            .append(v1)
+                            .append("\" -- \"")
+                            .append(v2)
+                            .append("\";\n");
+                }
+            }
+        }
+
+        sb.append("}");
+
+        return sb.toString();
+    }
 }
